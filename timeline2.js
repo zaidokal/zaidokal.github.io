a1ed06b5e42689c5ac3c588118a4165d547b9aa4
Zaid Okal <zokal@uwo.ca>
2023-07-09 02:41:19 -0400

    Script to navigate to a certain div on the timeline

--- timeline2.js
+++ timeline2.js
@@ -1,60 +1,65 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var mySwiper = new Swiper(".swiper-container", {
-    autoHeight: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
+var timelineSwiper = new Swiper(".timeline .swiper-container", {
+  direction: "vertical",
+  loop: false,
+  speed: 1600,
+  pagination: ".swiper-pagination",
+  paginationBulletRender: function (swiper, index, className) {
+    var year = document
+      .querySelectorAll(".swiper-slide")
+      [index].getAttribute("data-year");
+    return '<span class="' + className + '">' + year + "</span>";
+  },
+  paginationClickable: true,
+  nextButton: ".swiper-button-next",
+  prevButton: ".swiper-button-prev",
+  breakpoints: {
+    768: {
+      direction: "horizontal",
     },
-    speed: 500,
-    direction: "horizontal",
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      type: "progressbar",
-    },
-    loop: false,
-    effect: "slide",
-    spaceBetween: 30,
-    on: {
-      init: function () {
-        var paginationSwitches = document.querySelectorAll(
-          ".swiper-pagination-custom .swiper-pagination-switch"
-        );
-        paginationSwitches.forEach(function (switchElement, index) {
-          switchElement.classList.remove("active");
-          if (index === 0) {
-            switchElement.classList.add("active");
-          }
-        });
-      },
-      slideChangeTransitionStart: function () {
-        var paginationSwitches = document.querySelectorAll(
-          ".swiper-pagination-custom .swiper-pagination-switch"
-        );
-        paginationSwitches.forEach(function (switchElement, index) {
-          switchElement.classList.remove("active");
-          if (index === mySwiper.realIndex) {
-            switchElement.classList.add("active");
-          }
-        });
-      },
-    },
-  });
-
-  var switchElements = document.querySelectorAll(
-    ".swiper-pagination-custom .swiper-pagination-switch"
-  );
-  switchElements.forEach(function (switchElement) {
-    switchElement.addEventListener("click", function () {
-      var index = Array.from(switchElements).indexOf(this);
-      mySwiper.slideTo(index);
-      switchElements.forEach(function (switchElement) {
-        switchElement.classList.remove("active");
-      });
-      this.classList.add("active");
-    });
-  });
+  },
 });
+
+function navigateTimeline(year) {
+  // Find the index of the slide with the specified year
+  const slides = Array.from(
+    document.querySelectorAll(".timeline .swiper-slide")
+  );
+  const slideIndex = slides.findIndex(
+    (slide) => slide.getAttribute("data-year") === year.toString()
+  );
+
+  // Slide to the specified index
+  if (slideIndex !== -1) {
+    timelineSwiper.slideTo(slideIndex);
+  }
+}
+
+function clickTextInTimeline() {
+  var urlParams = new URLSearchParams(window.location.search);
+  var year = urlParams.get("year");
+  console.log(year);
+
+  var bullets = document.querySelectorAll(".swiper-pagination-bullet");
+  for (var i = 0; i < bullets.length; i++) {
+    if (bullets[i].textContent === year) {
+      bullets[i].click();
+      break;
+    }
+  }
+
+  // Remove the 'year' parameter from the URL
+  var newURL = window.location.pathname;
+  window.history.replaceState({}, document.title, newURL);
+}
+
+function goToTimeline() {
+  // Find all the <a> tags with the class "link-wrapper"
+  var linkWrappers = document.querySelectorAll(".link-wrapper");
+
+  // Set the href attribute of each <a> tag dynamically
+  linkWrappers.forEach(function (linkWrapper) {
+    var year = linkWrapper.parentElement.dataset.year;
+    linkWrapper.href = "timeline2.html?year=" + year;
+    linkWrapper.addEventListener("click", goToTimeline);
+  });
+}
---
+++ viewer.js
@@ -0,0 +1,8 @@
+// Get a reference to the button element
+var viewSquaresButton = document.getElementById("viewSquaresButton");
+
+// Add a click event listener to the button
+viewSquaresButton.addEventListener("click", function () {
+  // Redirect to the page that displays the squares
+  window.location.href = "viewer.html";
+});
