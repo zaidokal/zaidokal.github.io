992fb4f06644d8305495d45eb8343d888ae7849f
Zaid Okal <90659286+zaidokal@users.noreply.github.com>
2024-11-12 02:28:54 -0500

    Trying to fix taiwing config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,9 +3,9 @@ import type { Config } from "tailwindcss";
 export default {
   important: true,
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "@/src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "@/src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "@/src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
