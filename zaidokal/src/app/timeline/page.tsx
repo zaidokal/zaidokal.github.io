7411d5e7bf417bda52039d462509281c7847eaa2
Zaid Okal <90659286+zaidokal@users.noreply.github.com>
2024-11-12 01:05:00 -0500

    Removing unused definitions

--- zaidokal/src/app/timeline/page.tsx
+++ zaidokal/src/app/timeline/page.tsx
@@ -1,6 +1,5 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import TimelineItem from "@/components/TimelineItem";
 
 export default function TimelinePage() {
   return (
