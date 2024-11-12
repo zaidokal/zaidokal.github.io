5157c15bc88ac398901059d832b3555bc923302f
Zaid Okal <90659286+zaidokal@users.noreply.github.com>
2024-11-12 02:24:24 -0500

    Using important: true

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  important: true,
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
