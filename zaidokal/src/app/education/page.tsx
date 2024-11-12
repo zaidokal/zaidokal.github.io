8a4a7501e14142bc9da2b8767e24f4a80a852aa2
Zaid Okal <90659286+zaidokal@users.noreply.github.com>
2024-11-12 01:03:21 -0500

    Coming soon to education and timeline

--- zaidokal/src/app/education/page.tsx
+++ zaidokal/src/app/education/page.tsx
@@ -3,19 +3,17 @@ import Footer from "@/components/Footer";
 
 export default function EducationPage() {
   return (
-    <div className="min-h-screen bg-gray-100">
-      {/* Header */}
+    <main
+      className="flex flex-col h-screen w-full bg-cover bg-no-repeat bg-center"
+      style={{ backgroundImage: "url(/Pictures/BackPic.jpg)" }}
+    >
       <Header />
 
-      {/* Main Content */}
-      <main className="p-4">
-        <h1 className="text-2xl font-bold mb-4">Education</h1>
-        {/* Add your education content here */}
-        <p>Coming soon...</p>
-      </main>
+      <div className="flex flex-1 items-center justify-center overflow-auto gap-20 text-[100px]">
+        COMING SOON
+      </div>
 
-      {/* Footer */}
       <Footer />
-    </div>
+    </main>
   );
 }
