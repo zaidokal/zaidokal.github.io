1eddcd9bdb38201e36ac634e23ed2b0cbadb927d
Zaid Okal <90659286+zaidokal@users.noreply.github.com>
2024-11-12 02:12:50 -0500

    Commenting out nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "export",
-  distDir: "dist",
-  images: { unoptimized: true },
-};
+// const nextConfig: NextConfig = {
+//   output: "export",
+//   distDir: "dist",
+//   images: { unoptimized: true },
+// };
 
-export default nextConfig;
+// export default nextConfig;
