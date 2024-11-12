994123016f89da321b10ea6180dbbd4ef36ab6f0
Zaid Okal <90659286+zaidokal@users.noreply.github.com>
2024-11-12 03:05:44 -0500

    Attempted fx

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,6 @@ const nextConfig: NextConfig = {
   output: "export",
   distDir: "dist",
   images: { unoptimized: true },
-  basePath: isProd ? "/zaidokal.github.io" : "",
-  assetPrefix: isProd ? "/zaidokal.github.io/" : "",
 };
 
 export default nextConfig;
