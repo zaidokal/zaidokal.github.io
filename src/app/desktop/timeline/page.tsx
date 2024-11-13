17749a97381b1dc4707b5ce8a5dd5daac758008b
Zaid Okal <90659286+zaidokal@users.noreply.github.com>
2024-11-13 02:00:00 -0500

    Timeline page created, holding timelinebox components with data in timeline.json. Clicking on a timelinebox goes to the viewer page displaying it in full screen

--- src/app/desktop/timeline/page.tsx
+++ src/app/desktop/timeline/page.tsx
@@ -2,20 +2,50 @@
 
 import Header from "@/components/Desktop/Header";
 import Footer from "@/components/Desktop/Footer";
+import TimelineBox from "@/components/Desktop/TimelineBox";
+import { useEffect, useState } from "react";
 
-export default function DesktopTimelinePage() {
+interface TimelineData {
+  id: string;
+  year: string;
+  title: string;
+  description: string;
+  category: string;
+  image: string;
+}
+
+const DesktopTimelinePage: React.FC = () => {
+  const [timelineData, setTimelineData] = useState<TimelineData[]>([]);
+
+  useEffect(() => {
+    fetch("/data/timeline.json")
+      .then((response) => response.json())
+      .then((data) => setTimelineData(data));
+  }, []);
+
   return (
     <main
-      className="flex flex-col h-screen w-full bg-cover bg-no-repeat bg-center"
+      className="flex flex-col min-h-screen bg-cover bg-no-repeat bg-center"
       style={{ backgroundImage: "url(/Pictures/BackgroundPic.jpg)" }}
     >
       <Header />
 
-      <div className="flex flex-1 items-center justify-center overflow-auto gap-20 text-[200px]">
-        Coming Soon
+      <div className="flex flex-1 flex-wrap justify-center items-center p-10">
+        {timelineData.map((item) => (
+          <TimelineBox
+            key={item.id}
+            id={item.id}
+            year={item.year}
+            title={item.title}
+            description={item.description}
+            image={item.image}
+          />
+        ))}
       </div>
 
       <Footer />
     </main>
   );
-}
+};
+
+export default DesktopTimelinePage;
