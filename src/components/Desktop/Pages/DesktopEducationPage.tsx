1071db493f2509351b54ebc53e216367e60da7ae
Zaid Okal <90659286+zaidokal@users.noreply.github.com>
2024-11-17 22:09:39 -0500

    Added Extracurriculars section

--- src/components/Desktop/Pages/DesktopEducationPage.tsx
+++ src/components/Desktop/Pages/DesktopEducationPage.tsx
@@ -3,6 +3,8 @@ import IntroSection from "@/components/Desktop/Education/IntroSection";
 import EducationSection from "@/components/Desktop/Education/EducationSection";
 import TechnologiesSection from "@/components/Desktop/Education/TechnologiesSection";
 import technologies from "@/data/technologiesData";
+import ExtracurricularsSection from "@/components/Desktop/Education/ExtracurricularsSection";
+import Extracurriculars from "@/data/ExtracurricularsData";
 
 const DesktopEducationPage: React.FC = () => {
   return (
@@ -10,6 +12,7 @@ const DesktopEducationPage: React.FC = () => {
       <IntroSection />
       <EducationSection />
       <TechnologiesSection technologies={technologies} />
+      <ExtracurricularsSection Extracurriculars={Extracurriculars} />
     </div>
   );
 };
---
+++ src/data/ExtracurricularsData.ts
@@ -0,0 +1,10 @@
+const Extracurriculars = [
+  { title: "Aero Design Western", color: "from-blue-400 to-blue-600" },
+  { title: "We Bots", color: "from-purple-400 to-purple-600" },
+  { title: "WEC", color: "from-green-400 to-green-600" },
+  { title: "WEC Programming", color: "from-yellow-400 to-yellow-600" },
+  { title: "Hack Western 8", color: "from-green-400 to-green-600" },
+  { title: "Hack Western 9", color: "from-blue-400 to-blue-600" },
+];
+
+export default Extracurriculars;
