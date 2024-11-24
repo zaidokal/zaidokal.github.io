56d1f08f549ea5e5efeded4f04da4f5c0c14ebe5
Zaid Okal <90659286+zaidokal@users.noreply.github.com>
2024-11-24 03:27:36 -0500

    Showing only favourites in timeline

--- src/components/Desktop/Pages/DesktopTimelinePage.tsx
+++ src/components/Desktop/Pages/DesktopTimelinePage.tsx
@@ -8,6 +8,7 @@ interface TimelineData {
   description: string;
   category: string;
   image: string;
+  favourites: boolean;
 }
 
 const DesktopTimelinePage: React.FC = () => {
@@ -19,10 +20,12 @@ const DesktopTimelinePage: React.FC = () => {
       .then((data) => setTimelineData(data));
   }, []);
 
+  const favouriteTimelineData = timelineData.filter((item) => item.favourites);
+
   return (
     <div className="flex flex-wrap items-center justify-center p-10 pb-[40px] pt-[100px]">
       <div className="relative z-10 grid grid-cols-1 gap-10 px-10 py-20 md:grid-cols-2 lg:grid-cols-3">
-        {timelineData.map((item) => (
+        {favouriteTimelineData.map((item) => (
           <TimelineBox
             key={item.id}
             id={item.id}
